1e9b5b2646e0a6ba6d2722cd51e7a3a2ea39923c
Joseph Timbol <joseph2000k@gmail.com>
2021-06-23 22:37:53 +0800

    Request & Body Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-//@route    GET api/users
-//@desc     Test route
+//@route    POST api/users
+//@desc     Register route
 //@access   Public
-router.get('/', (req, res)=> res.send('user route'));
+router.post('/', [
+    body('name', 'Name is required').not().isEmpty(),
+    body('email', 'Please includeb a valid email').isEmail(),
+    body('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+], (req, res)=> {
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    res.send('user route');
+});
 
 module.exports = router;
