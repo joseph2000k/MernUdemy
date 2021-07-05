328305f4786b69d6ab515fb767710fca5695af9e
Joseph Timbol <joseph2000k@gmail.com>
2021-07-05 11:14:12 +0800

    test commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
-const { check, validationResult } = require("express-validator");
-
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
@@ -26,40 +24,5 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
-
-//@route    POST api/Profile
-//@desc     Create or update user profile
-//@access   Private
-router.post(
-  "/",
-  [
-    auth,
-    [
-      check("status", "Status is required").not().isEmpty(),
-      check("skills", "Skills is required").not().isEmpty(),
-    ],
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-
-    const {
-      company,
-      website,
-      location,
-      bios,
-      skills,
-      youtube,
-      twitter,
-      instagram,
-      linkedin,
-      facebook,
-      // spread the rest of the fields we don't need to check
-      ...rest
-    } = req.body;
-  }
-);
 
 module.exports = router;
