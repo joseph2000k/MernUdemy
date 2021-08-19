608104a8001bbc12126531f87a8d641678315b67
Joseph Timbol <joseph2000k@gmail.com>
2021-08-19 22:21:40 +0800

    Adding Posts

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,9 +1,10 @@
-import React, { Fragment, useEffect } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import Spinner from "../layout/Spinner";
-import PostItem from "./PostItem";
-import { getPosts } from "../../actions/post";
+import React, { Fragment, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
+import { getPosts } from '../../actions/post';
+import PostForm from './PostForm';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
@@ -14,12 +15,12 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <Spinner />
   ) : (
     <Fragment>
-      <h1 className="large text-primary">Posts</h1>
-      <p className="lead">
-        <i className="fas fa-user">Welcome to the community</i>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <i className='fas fa-user'>Welcome to the community</i>
       </p>
-      {/* PostForm */}
-      <div className="posts">
+      <PostForm />
+      <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
         ))}
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,8 +1,10 @@
+import { post } from 'request';
 import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
+  ADD_POST,
 } from '../actions/types';
 
 const initialState = {
@@ -22,6 +24,14 @@ export default function postReducer(state = initialState, action) {
         posts: payload,
         loading: false,
       };
+
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [payload, ...state.posts],
+        loading: false,
+      };
+
     case DELETE_POST:
       return {
         ...state,
